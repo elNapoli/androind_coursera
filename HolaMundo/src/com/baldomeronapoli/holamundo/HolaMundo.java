486dea39df0639e554cb59d34c360b0863508fa3
elnapoli <baldomero.napoli@gmail.com>
2017-09-05 14:26:23 -0300

    proyecot creado con IDE exlipse

---
+++ HolaMundo/src/com/baldomeronapoli/holamundo/HolaMundo.java
@@ -0,0 +1,22 @@
+package com.baldomeronapoli.holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo =D");
+		//primitivos
+		/*
+		 * byte
+		 * short
+		 * int
+		 * long
+		 * 
+		 * */
+		/*
+		 * 
+		 * */
+
+	}
+
+}
